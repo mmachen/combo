ad25415708c0e177a3baa53930bc5e9e4766e656
Michael Machen <machen.michael@gmail.com>
2020-07-05 17:38:35 -0500

    seet

--- houseSearchSite/settings.py
+++ houseSearchSite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   'staticfiles'
 
 STATIC_URL = '/static/'
 
